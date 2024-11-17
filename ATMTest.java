02186db01e6a62834bb2dee7f5026da8a04cde1b
tb-sirait <tbsintheworld@gmail.com>
2024-11-17 23:01:53 +0700

    perubahan

--- ATMTest.java
+++ ATMTest.java
@@ -9,19 +9,16 @@ public class ATMTest {
 
     @BeforeMethod
     public void setUp() {
-        // Inisialisasi objek ATM dengan saldo awal sebesar 1000
         atm = new atm(1000);
     }
 
     @Test
     public void testLihatSaldo() {
-        // Menguji apakah metode lihatSaldo() mengembalikan saldo yang benar
         Assert.assertEquals(atm.lihatSaldo(), 1000.0, "Saldo harus 1000");
     }
 
     @Test
-    public void testSetorUang() {
-        // Menguji apakah metode setorUang() berfungsi dengan benar ketika jumlah yang valid disetor
+    public void testSetorUang() { 
         atm.setorUang(500);
         Assert.assertEquals(atm.lihatSaldo(), 1500.0, "Saldo harus 1500 setelah setor 500");
     }
@@ -29,13 +26,11 @@ public class ATMTest {
     @Test(expectedExceptions = IllegalArgumentException.class, 
           expectedExceptionsMessageRegExp = "Jumlah harus lebih besar dari 0 ")
     public void testSetorUangNegatif() {
-        // Menguji apakah metode setorUang() melempar pengecualian ketika jumlah yang negatif disetor
         atm.setorUang(0);
     }
 
     @Test
     public void testTarikUang() {
-        // Menguji apakah metode tarikUang() berfungsi dengan benar ketika jumlah yang valid ditarik
         atm.tarikUang(300);
         Assert.assertEquals(atm.lihatSaldo(), 700.0, "Saldo harus 700 setelah tarik 300");
     }
@@ -43,14 +38,12 @@ public class ATMTest {
     @Test(expectedExceptions = IllegalArgumentException.class, 
           expectedExceptionsMessageRegExp = "Saldo tidak mencukupi ")
     public void testTarikUangLebihDariSaldo() {
-        // Menguji apakah metode tarikUang() melempar pengecualian ketika jumlah yang ditarik melebihi saldo saat ini
         atm.tarikUang(1200);
     }
 
     @Test(expectedExceptions = IllegalArgumentException.class, 
           expectedExceptionsMessageRegExp = "Jumlah harus lebih besar dari 0 ")
     public void testTarikUangNegatif() {
-        // Menguji apakah metode tarikUang() melempar pengecualian ketika jumlah yang negatif ditarik
         atm.tarikUang(-200);
     }
 }
